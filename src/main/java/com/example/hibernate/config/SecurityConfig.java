ae1dfd3e194529ffcc1bda797e0e9f1c2caf6897
Ekaterina Kovalenko <katrin.kovali2702@gmail.com>
2023-01-16 17:40:21 +0400

    method-security

--- src/main/java/com/example/hibernate/config/SecurityConfig.java
+++ src/main/java/com/example/hibernate/config/SecurityConfig.java
@@ -2,31 +2,24 @@ package com.example.hibernate.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true,jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         @Override
         protected void configure(AuthenticationManagerBuilder auth) throws Exception {
             PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-            auth.inMemoryAuthentication().withUser("User").password(encoder.encode("password")).roles("USER");
-            auth.inMemoryAuthentication().withUser("Admin").password(encoder.encode("password")).roles("ADMIN");
-            auth.inMemoryAuthentication().withUser("Noname").password(encoder.encode("100")).roles("USER");
+            auth.inMemoryAuthentication().withUser("User").password(encoder.encode("password")).roles("WRITE");
+            auth.inMemoryAuthentication().withUser("Admin").password(encoder.encode("password")).roles("DELETE");
+            auth.inMemoryAuthentication().withUser("Noname").password(encoder.encode("100")).roles("READ");
         }
 
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .authorizeRequests().antMatchers("/persons/by-person", "/persons/by-city").permitAll()
-                    .and()
-                    .authorizeRequests().anyRequest().authenticated()
-                    .and()
-                    .formLogin();
-        }
 
 }
 
--- src/main/java/com/example/hibernate/controller/Controller.java
+++ src/main/java/com/example/hibernate/controller/Controller.java
@@ -2,10 +2,14 @@ package com.example.hibernate.controller;
 
 import com.example.hibernate.entity.Person;
 import com.example.hibernate.service.Service;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,20 +22,27 @@ public class Controller {
     }
 
     @GetMapping("/persons/by-city")
+    @Secured("ROLE_READ")
     public List<Person> getNameByCity(@RequestParam("city") String city) {
         return service.findByCityOfLiving(city);
     }
 
     @GetMapping("/persons/by-person")
+    @PreAuthorize("hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE')")
     public Optional<Person> getPersonByNameAndSurname(@RequestParam("name") String name,
                                                       @RequestParam("surname") String surname) {
         return service.findByPersonIdNameAndAndPersonIdSurname(name, surname);
     }
 
-
     @GetMapping("/persons/by-age")
+    @RolesAllowed("ROLE_WRITE")
     public List<Person> getPersonsByAge(@RequestParam("age") int age) {
         return service.findByPersonIdAge(age);
     }
 
+    @GetMapping("/username")
+        @PreAuthorize("#username == authentication.principal.username")
+    public String login (String username) {
+        return username + ", you got access";
+    }
 }
